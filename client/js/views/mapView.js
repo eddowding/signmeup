cfb449dd212b5c4bd3c5d76355df000e6bde75bd
Sym Roe <sym.roe@talusdesign.co.uk>
2013-02-07 12:30:19 +0000

    Fix projection

--- client/js/views/mapView.js
+++ client/js/views/mapView.js
@@ -73,7 +73,7 @@ define([
             
             var osgbTransform = new L.Transformation(1, 0, -1, 0);
             var osgbCrs = L.CRS.proj4js('EPSG:27700', 
-            "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs", 
+            "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-97910 +ellps=airy +datum=OSGB36 +units=m +no_defs", 
             osgbTransform);
 
             var scaleFn = function(zoom) {
